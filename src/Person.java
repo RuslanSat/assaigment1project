9d4f0d78781053c4e5256f25420e250ca27ed09d
RuslanSat <ruslansatvalidev1@gmail.com>
2024-12-28 12:34:37 +0500

    assaigment 1 1 task

---
+++ src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private boolean gender;
+
+    public Person(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        String genderStr = gender ? "Male" : "Female";
+        return "Hi, I am " + name + " " + surname + ", a " + age + "-year-old " + genderStr + ".";
+    }
+
+     public static void main(String[] args) {
+         Person person = new Person("Severus", "Snape", 51, true);
+        System.out.println(person);
+     }
+}
